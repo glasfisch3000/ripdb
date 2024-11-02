72eeeb7552e8ddf91b67b72380c1284c5fffcee6
glasfisch3000 <jakob@danckwerts.net>
2024-11-02 05:20:10 +0100

    change data parsing from base64 to hex

--- Sources/RipDB/utils/Data.swift
+++ Sources/RipDB/utils/Data.swift
@@ -1,8 +1,13 @@
-import Foundation
 import ArgumentParser
+import Vapor
 
 extension Data: @retroactive ExpressibleByArgument {
     public init?(argument: String) {
-        self.init(base64Encoded: argument)
+        if var buffer = try? ByteBuffer(plainHexEncodedBytes: argument),
+            let data = buffer.readData(length: buffer.readableBytes) {
+            self = data
+        } else {
+            return nil
+        }
     }
 }
