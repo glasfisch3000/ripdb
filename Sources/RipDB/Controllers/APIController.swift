df5b3fd08e2a64e66d17b6741770d6f51c6ea565
glasfisch3000 <jakob@danckwerts.net>
2024-11-11 22:10:46 +0100

    add API list functions

--- Sources/RipDB/Controllers/APIController.swift
+++ Sources/RipDB/Controllers/APIController.swift
@@ -11,46 +11,81 @@ struct APIController: RouteCollection {
         var id: UUID
     }
     
-    struct LocationContext: Codable {
+    struct LocationsListContext: Codable {
+        var locations: [LocationDTO.WithHexHash]
+    }
+    
+    struct LocationsGetContext: Codable {
         var id: UUID
         var location: LocationDTO.WithHexHash
     }
     
-    struct FileContext: Codable {
+    struct FilesListContext: Codable {
+        var files: [FileDTO.WithHexHash]
+    }
+    
+    struct FilesGetContext: Codable {
         var id: UUID
         var file: FileDTO.WithHexHash
     }
     
-    struct VideoContext: Codable {
+    struct VideosListContext: Codable {
+        var videos: [VideoDTO.WithHexHash]
+    }
+    
+    struct VideosGetContext: Codable {
         var id: UUID
         var video: VideoDTO.WithHexHash
     }
     
-    struct ProjectContext: Codable {
+    struct ProjectsListContext: Codable {
+        var projects: [ProjectDTO.WithHexHash]
+    }
+    
+    struct ProjectsGetContext: Codable {
         var id: UUID
         var project: ProjectDTO.WithHexHash
     }
     
-    struct CollectionContext: Codable {
+    struct CollectionsListContext: Codable {
+        var collections: [CollectionDTO.WithHexHash]
+    }
+    
+    struct CollectionsGetContext: Codable {
         var id: UUID
         var collection: CollectionDTO.WithHexHash
     }
     
     func boot(routes: any RoutesBuilder) throws {
         let locations = routes.grouped("locations")
+        locations.get(use: locationsList(request:))
         locations.get(":id", use: locationsGet(request:))
         
         let files = routes.grouped("files")
+        files.get(use: filesList(request:))
         files.get(":id", use: filesGet(request:))
         
         let videos = routes.grouped("videos")
+        videos.get(use: videosList(request:))
         videos.get(":id", use: videosGet(request:))
         
         let projects = routes.grouped("projects")
+        projects.get(use: projectsList(request:))
         projects.get(":id", use: projectsGet(request:))
         
         let collections = routes.grouped("collections")
+        collections.get(use: collectionsList(request:))
         collections.get(":id", use: collectionsGet(request:))
+    }
+    
+    @Sendable
+    func locationsList(request req: Request) async throws -> View {
+        let locations = try await Location.query(on: req.db)
+            .sort(\.$name)
+            .all()
+        
+        let context = LocationsListContext(locations: locations.map { $0.toDTO().toHexHash() })
+        return try await req.view.render("locations", context)
     }
     
     @Sendable
@@ -71,8 +106,31 @@ struct APIController: RouteCollection {
         .flatten(on: req.eventLoop)
         .get()
         
-        let context = LocationContext(id: id, location: location.toDTO().toHexHash())
+        let context = LocationsGetContext(id: id, location: location.toDTO().toHexHash())
         return try await req.view.render("location", context)
+    }
+    
+    @Sendable
+    func filesList(request req: Request) async throws -> View {
+        let files = try await File.query(on: req.db)
+            .with(\.$location)
+            .with(\.$video) {
+                $0.with(\.$project)
+            }
+            .join(parent: \.$location)
+            .join(parent: \.$video)
+            .join(from: Video.self, parent: \.$project)
+            .sort(Project.self, \.$name)
+            .sort(Video.self, \.$type)
+            .sort(Video.self, \.$name)
+            .sort(\.$resolution)
+            .sort(\.$is3D)
+            .sort(Location.self, \.$name)
+            .sort(\.$size)
+            .all()
+        
+        let context = FilesListContext(files: files.map { $0.toDTO().toHexHash() })
+        return try await req.view.render("files", context)
     }
     
     @Sendable
@@ -91,8 +149,22 @@ struct APIController: RouteCollection {
         }
         .get()
         
-        let context = FileContext(id: id, file: file.toDTO().toHexHash())
+        let context = FilesGetContext(id: id, file: file.toDTO().toHexHash())
         return try await req.view.render("file", context)
+    }
+    
+    @Sendable
+    func videosList(request req: Request) async throws -> View {
+        let videos = try await Video.query(on: req.db)
+            .with(\.$project)
+            .join(parent: \.$project)
+            .sort(Project.self, \.$name)
+            .sort(\.$type)
+            .sort(\.$name)
+            .all()
+        
+        let context = VideosListContext(videos: videos.map { $0.toDTO().toHexHash() })
+        return try await req.view.render("videos", context)
     }
     
     @Sendable
@@ -112,8 +184,23 @@ struct APIController: RouteCollection {
         .flatten(on: req.eventLoop)
         .get()
         
-        let context = VideoContext(id: id, video: video.toDTO().toHexHash())
+        let context = VideosGetContext(id: id, video: video.toDTO().toHexHash())
         return try await req.view.render("video", context)
+    }
+    
+    @Sendable
+    func projectsList(request req: Request) async throws -> View {
+        let projects = try await Project.query(on: req.db)
+            .with(\.$collection)
+            .join(parent: \.$collection)
+            .sort(CollectionModel.self, \.$name)
+            .sort(\.$releaseYear)
+            .sort(\.$name)
+            .sort(\.$type)
+            .all()
+        
+        let context = ProjectsListContext(projects: projects.map { $0.toDTO().toHexHash() })
+        return try await req.view.render("projects", context)
     }
     
     @Sendable
@@ -129,8 +216,18 @@ struct APIController: RouteCollection {
         try await project.$collection.load(on: req.db)
         try await project.$videos.load(on: req.db)
         
-        let context = ProjectContext(id: id, project: project.toDTO().toHexHash())
+        let context = ProjectsGetContext(id: id, project: project.toDTO().toHexHash())
         return try await req.view.render("project", context)
+    }
+    
+    @Sendable
+    func collectionsList(request req: Request) async throws -> View {
+        let collections = try await CollectionModel.query(on: req.db)
+            .sort(\.$name)
+            .all()
+        
+        let context = CollectionsListContext(collections: collections.map { $0.toDTO().toHexHash() })
+        return try await req.view.render("collections", context)
     }
     
     @Sendable
@@ -145,7 +242,7 @@ struct APIController: RouteCollection {
         
         try await collection.$projects.load(on: req.db)
         
-        let context = CollectionContext(id: id, collection: collection.toDTO().toHexHash())
+        let context = CollectionsGetContext(id: id, collection: collection.toDTO().toHexHash())
         return try await req.view.render("collection", context)
     }
 }
